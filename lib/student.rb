306aa2bc25c39001ac782e9266538893fd7e131a
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-15 21:01:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,10 +45,10 @@ class Student
     end
   end
 
-  def self.create(name:, grade:)
+  def self.create(name, grade)
     student = self.new(name, grade)
     student.save
-    student
+    #sstudent
   end
 
   #row is an array of id, name, and grade
